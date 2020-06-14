b05bf2cf35f9c3792c42fa96a5dddc251d73e685
BrunoCAF <brunocaf123@gmail.com>
2020-06-14 18:34:07 -0300

    tem q passar o valor da orientação na hora de construir, mas se n passar ainda roda

--- src/truckdriver/marker.py
+++ src/truckdriver/marker.py
@@ -1,10 +1,13 @@
 import pyglet
 import pymunk
 class Marker:
-    def __init__(self, position : (float, float)):
+    def __init__(self, position : (float, float), vertical : bool = False):
         self.image = pyglet.image.load("rsc/marker.png")
         self.sprite = pyglet.sprite.Sprite(self.image, subpixel = True)
         self.position = pymunk.Vec2d(position)
+        self.vertical = vertical
+        if not vertical:
+            self.sprite.update(rotation=90.0)
 
     def update(self, delta : float):
         pass
@@ -12,8 +15,13 @@ class Marker:
     def check_win(self, container : pymunk.Body) -> bool:
         points = [container.local_to_world((-3,-12)), container.local_to_world((3,-12)), container.local_to_world((3,12)), container.local_to_world((-3,12))]
         won = True
+        (dx, dy) = (10, 26) if self.vertical else (26,-10)
+        limits = {'x': (min(self.position.x, self.position.x + dx), 
+                        max(self.position.x, self.position.x + dx)), 
+                  'y': (min(self.position.y, self.position.y + dy), 
+                        max(self.position.y, self.position.y + dy))}
         for point in points:
-            if point.x > self.position.x + 10 or point.x < self.position.x or point.y > self.position.y + 26 or point.y < self.position.y:
+            if (point.x < limits['x'][0] or point.x > limits['x'][1]) or (point.y < limits['y'][0] or point.y > limits['y'][1]):
                 won = False
         return won;
 
